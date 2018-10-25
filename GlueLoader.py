0b5b757dcace3caa80b9e92eea55304af15341cd
Aaron Chu <xweichu@hotmail.com>
2018-10-25 16:59:34 -0700

    reorg the files

--- GlueLoader.py
+++ GlueLoader.py
@@ -1,15 +1,16 @@
 import numpy as np
-
 from glue.core.component_id import ComponentID
 from glue.core.data import BaseCartesianData
 from glue.utils import view_shape
+from dask.distributed import Client
 
 
 class DaskData(BaseCartesianData):
 
-    def __init__(self):
+    def __init__(self, client):
         super(DaskData, self).__init__()
         self.data_cid = ComponentID(label='data', parent=self)
+        self.client = client
 
     @property
     def label(self):
@@ -29,38 +30,12 @@ class DaskData(BaseCartesianData):
     @property
     def ndim(self):
         return len(self.shape)
-    
-    
-    def get_mask(self, subset_state, view=None):
-        return subset_state.to_mask(self, view=view)
-
 
     def get_data(self, cid, view=None):
-
-        # if cid in self.pixel_component_ids:
-        #     shape = tuple(-1 if i == cid.axis else 1 for i in range(self.ndim))
-        #     pix = np.arange(self.shape[cid.axis], dtype=float).reshape(shape)
-        #     return broadcast_to(pix, self.shape)[view]
-        # else:
-        #     raise IncompatibleAttribute(cid)
-            
-        # if cid in self.pixel_component_ids:
-        #     return super(DaskData, self).get_data(cid, view=view)
-        # else:
         from dask.distributed import Client
         import dask.delayed
         import dask.array as da
-        client = client = Client('128.104.222.103:8786')
-
-        # def calc_random(view):
-        #     import dask.array as da
-        #     import numpy as np
-        #     from glue.utils import view_shape
-        #     x = da.from_zarr('/mnt/cephfs/zarr_data_full')
-        #     z = x[2300]
-        #     z = z[view]
-        #     z = z.compute()
-        #     return z
+        client = self.client
 
         def load_ceph_data(view):
             import dask.array as da
@@ -88,16 +63,37 @@ class DaskData(BaseCartesianData):
             z = z.compute()
             return z
 
-        # client.restart()
         future = client.submit(load_ceph_data, view)
         result = future.result()
-        print("function triggered")
         return result 
 
     def get_mask(self, subset_state, view=None):
         print("get mask triggered")
         return subset_state.to_mask(self, view=view)
 
+
+    # def compute_statistic(self, statistic, cid,
+    #                       axis=None, finite=True,
+    #                       positive=False, subset_state=None,
+    #                       percentile=None, random_subset=None):
+
+    #     if statistic == 'minimum':
+    #         return 0 #da.min(z,axis).compute()
+    #     elif statistic == 'maximum':
+    #         return 1 #da.max(z,axis).compute()
+    #     elif statistic == 'mean' or statistic == 'median':
+    #         return 0.5 #da.mean(z,axis).compute()
+    #     elif statistic == 'percentile':
+    #         return percentile/100
+    #         # return da.percentile(z,percentile)
+    #     elif statistic == 'sum':
+    #         return self.size/2 #da.sum(z.axis).compute()
+
+    #         # final_shape = tuple(self.shape[i] for i in range(self.ndim)
+    #         #                     if i not in axis)
+    #         # return np.random.random(final_shape)
+    #         # return 0
+    #     return 0
 
     def compute_statistic(self, statistic, cid,
                           axis=None, finite=True,
@@ -108,8 +104,7 @@ class DaskData(BaseCartesianData):
         from dask.distributed import Client
         import dask.delayed
         import dask.array as da
-        client = client = Client('128.104.222.103:8786')
-
+        client = self.client
 
         def load_data(statistic, axis):
             import dask.array as da
@@ -140,78 +135,61 @@ class DaskData(BaseCartesianData):
                 return da.mean(z,axis).compute()
             elif statistic == 'percentile':
                 return percentile/100
-                # return da.percentile(z,percentile)
             elif statistic == 'sum':
                 return da.sum(z.axis).compute()
-
-                # final_shape = tuple(self.shape[i] for i in range(self.ndim)
-                #                     if i not in axis)
-                # return np.random.random(final_shape)
-                # return 0
             return 0
         
         future = client.submit(load_data, statistic,axis)
         result = future.result()
         print("compute statistics function triggered")
-        return result 
-
+        return result     
 
 
 
 
     def compute_histogram(self, cids,
-                          range1=None, bins=None, log=False,
+                          range=None, bins=None, log=False,
                           subset_state=None, subset_group=None):
         
         from dask.distributed import Client
         import dask.delayed
         import dask.array as da
-        client = client = Client('128.104.222.103:8786')
+        client = self.client
 
         def get_histogram():
             import dask.array as da
             import numpy as np
-            from glue.utils import view_shape
+
             x = da.from_zarr('/mnt/cephfs/zarr_data_full')
             f = 0
             scale = 10
-
+            rg = [0,1,2,3,4,5,6,7,8,9]
             lh = []
-            for k in range(scale):
+            for k in rg:
                 lc = []
-                for i in range(scale):
+                for i in rg:
                     lr = []
-                    for j in range(scale):
+                    for j in rg:
                         lr.append(x[f%3500])
                         f = f+1
                     lc.append(da.concatenate(lr))
                 lh.append(da.concatenate(lc,1))
             z = da.concatenate(lh,2)
-
-            # if view != None:
-            #     z = z[view]
-
-            # print("trst")
-
-            h , bins = da.histogram(z)
-
+            h , bins = da.histogram(z,bins=np.arange(100))
             h = h.compute()
             return h
-
+        
         future = client.submit(get_histogram)
         result = future.result()
-        print("function triggered 2")
         return result
 
 
-# We now create a data object using the above class,
-# and launch a a glue session
 
 from glue.core import DataCollection
 from glue.app.qt.application import GlueApplication
-# client = Client('10.0.1.9:8786')
+client = Client('128.104.222.103:8786')
 
-d = DaskData()
+d = DaskData(client)
 dc = DataCollection([d])
 ga = GlueApplication(dc)
 ga.start(maximized=False)
